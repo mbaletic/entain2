d001897fbb3cf0c69bafe0e5c16b8ab2997d5199
mbaletic <58534657+mbaletic@users.noreply.github.com>
2025-07-03 18:13:13 +0200

    Setup Use MSTest Use inheritance instead of DI

---
+++ Base.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entain2
+{
+    public class Base
+    {
+        public HttpClient httpClient;
+        public Client client;
+
+        public Base()
+        {
+            httpClient = new HttpClient();
+            client = new Client(httpClient);
+        }
+
+        [AssemblyCleanup]
+        public void TearDown()
+        {
+
+            httpClient.Dispose();
+
+        }
+
+
+    }
+}
--- Test1.cs
+++ Test1.cs
@@ -1,11 +1,17 @@
-﻿namespace entain2
+﻿
+namespace entain2
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class Test1 : Base
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestMethod1Async()
         {
+            var response = await client.GetPetByIdAsync(100);
+
+            Console.WriteLine(response.Name);
         }
     }
 }
+
+
